6250dcc8d26f1aa8802d9dd8da574c61b4d83741
juanburillo <juanburillodev@gmail.com>
2024-04-19 10:37:29 +0200

    Set up fresh Spring Boot project

---
+++ src/test/java/com/izertis/grouPay/GrouPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izertis.grouPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrouPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
