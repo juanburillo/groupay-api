c0f3ad996f714774b697f6d941d4928104c142de
juanburillo <juanburillodev@gmail.com>
2024-04-23 11:31:42 +0200

    Fix MapStruct not working

--- src/main/java/com/izertis/grouPay/expense/infrastructure/ExpenseMapper.java
+++ src/main/java/com/izertis/grouPay/expense/infrastructure/ExpenseMapper.java
@@ -4,7 +4,9 @@ import com.izertis.grouPay.expense.domain.Expense;
 import com.izertis.grouPay.expense.infrastructure.primaryadapter.rest.ExpenseRequest;
 import com.izertis.grouPay.expense.infrastructure.primaryadapter.rest.ExpenseResponse;
 import com.izertis.grouPay.expense.infrastructure.secondaryadapter.database.ExpenseEntity;
+import com.izertis.grouPay.friend.domain.Friend;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -14,10 +16,19 @@ public interface ExpenseMapper {
 
     ExpenseMapper INSTANCE = Mappers.getMapper(ExpenseMapper.class);
 
+    @Mapping(source = "friendId", target = "friend")
     Expense toModel(ExpenseRequest expenseRequest);
+
+    @Mapping(source = "friendId", target = "friend")
     Expense toModel(ExpenseEntity expenseEntity);
+
+    @Mapping(source = "friendId", target = "friend")
     List<Expense> toModelList(List<ExpenseEntity> expenseEntities);
+
     ExpenseResponse toDto(Expense expense);
+
     List<ExpenseResponse> toDtoList(List<Expense> expenses);
+
+    Friend map(Long id);
 
 }
