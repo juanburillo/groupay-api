f58aab7992078a0ee2e8a569e3a7376b3c0f8fc2
juanburillo <juanburillodev@gmail.com>
2024-04-29 10:57:27 +0200

    Document TransactionController with OpenAPI and Swagger

--- src/main/java/com/izertis/grouPay/transaction/infrastructure/primaryadapter/rest/TransactionController.java
+++ src/main/java/com/izertis/grouPay/transaction/infrastructure/primaryadapter/rest/TransactionController.java
@@ -1,6 +1,10 @@
 package com.izertis.grouPay.transaction.infrastructure.primaryadapter.rest;
 
 import com.izertis.grouPay.transaction.application.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,10 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+    @Operation(summary = "Get the minimum amount of payment transactions every friend has to perform to clear the group debt")
+    @ApiResponse(responseCode = "200", description = "Calculated and returned all transactions", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = TransactionResponse.class))
+    })
     @GetMapping
     public List<TransactionResponse> getTransactions() {
         return TransactionMapper.INSTANCE.toDtoList(transactionService.getTransactions());
