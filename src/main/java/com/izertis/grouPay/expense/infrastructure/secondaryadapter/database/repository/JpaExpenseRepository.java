4c76818638465212576b43f3b98ff52916a5d21f
juanbburillo <juanburillodev@gmail.com>
2024-05-07 15:43:27 +0200

    Fix null friend name bug

--- src/main/java/com/izertis/grouPay/expense/infrastructure/secondaryadapter/database/repository/JpaExpenseRepository.java
+++ src/main/java/com/izertis/grouPay/expense/infrastructure/secondaryadapter/database/repository/JpaExpenseRepository.java
@@ -2,8 +2,15 @@ package com.izertis.grouPay.expense.infrastructure.secondaryadapter.database.rep
 
 import com.izertis.grouPay.expense.infrastructure.secondaryadapter.database.entity.ExpenseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
+    @Query(
+            value = "SELECT name FROM friend JOIN expense ON friend.id = friend_id WHERE expense.id = :id",
+            nativeQuery = true
+    )
+    String findExpenseFriendNameById(@Param("id") Long id);
 }
